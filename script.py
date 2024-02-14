41dc669d112dea4a39da6fbda4925f81bf3b34d0
calebkreed <73898417+calebkreed@users.noreply.github.com>
2024-02-14 10:41:13 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,19 @@
+import cv2
 
+cap = cv2.Videocapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Check if the frame was captured successfully
+    if not ret:
+        print("Error: Failed to capture frame.")
+        break
+
+    # Display the captured frame
+    cv2.imshow('Camera', frame)
+
+    # Check for the 'q' key to quit the program
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
